893cd34f13a4b76f6292c91f3ca44aac9311c70b
Yassine <79906628+Yassine952@users.noreply.github.com>
2022-06-19 01:36:05 +0200

    Create stylesheet.sh

---
+++ stylesheet.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+if [ "$1" = "--add-stylesheet" ]; then
+
+	#check if url is empty
+	if [ -z "$2" ]; then
+		echo "Error: url is empty"
+	else    
+		URL=$2
+	fi
+
+	#Check if filename is empty
+	if [ -z "$3" ]; then
+		echo "Error: filename is empty"
+	else    
+    	FILENAME=$3
+  	fi
+
+	# rights checks  
+  	if [ ! -w "$FILENAME" ]; then
+	  echo "Error: not permission"
+	fi
+
+
+	#Insert link tag if there are not a link tag 
+	if ! grep -q "<link>" $FILENAME; then
+		sed -i 'i\<head> <link rel="stylesheet" href="'$URL'"> ' $FILENAME
+
+		
+	fi
+	
+	exit 0
+
+
+else
+	echo "Usage: stylesheet.sh --add-stylesheet URL FILENAME"
+fi
